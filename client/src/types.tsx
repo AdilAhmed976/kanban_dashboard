c742e69c6995375f719a645ce9132059a5c9d287
AdilAhmed976 <adilahmedah976@gmail.com>
2023-11-07 18:07:53 +0530

    types file added

---
+++ client/src/types.tsx
@@ -0,0 +1,12 @@
+export interface todoObj {
+  id: string;
+  name: string;
+  description: string;
+  status: string;
+}
+
+export interface boardsObj {
+  id: string;
+  name: string;
+  description: string;
+}
